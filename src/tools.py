e1ecc49d1cde6879be72f78d403bfbc74d26b4c2
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-21 17:39:43 +0330

    commit-5

--- src/tools.py
+++ src/tools.py
@@ -4,18 +4,18 @@ from os import listdir
 
 def unarchive(chat_id, file_name, password):
 
-    file_adress = f"/downloads/{file_name}"
+    file_adress = f"/home/container/downloads/{file_name}"
     
     if password == "the file is not locked":
-        patoolib.extract_archive(file_adress, outdir=f"/users_file/{chat_id}/files/")
+        patoolib.extract_archive(file_adress, outdir=f"/home/container/users_file/{chat_id}/files/")
     else:
-        patoolib.extract_archive(file_adress, outdir=f"/users_file/{chat_id}/files/")
+        patoolib.extract_archive(file_adress, outdir=f"/home/container/users_file/{chat_id}/files/")
     
-    files_list = listdir(f"/users_file{chat_id}/files/")
+    files_list = listdir(f"/home/container/users_file{chat_id}/files/")
     
     final_file_adresses = []
     for file in files_list:
         
-        final_file_adresses.append(f"/users_file/{chat_id}/files/{file}")
+        final_file_adresses.append(f"/home/container/users_file/{chat_id}/files/{file}")
         
     return final_file_adresses
