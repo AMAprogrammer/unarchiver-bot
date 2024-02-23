94552b0ca1086960fcbf4ab94afd0ac90b33d6d1
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-23 12:23:13 +0330

    commit-6

--- src/tools.py
+++ src/tools.py
@@ -1,19 +1,21 @@
-from os import listdir
+from os import listdir, mkdir
 import pyunpack
 def unarchive(chat_id, file_name, password):
 
+    mkdir()
     file_adress = f"/home/container/downloads/{file_name}"
+    mkdir(path=f"home/container/users_file/{chat_id}")
     
     if password == "the file is not locked":
-        pyunpack.Archive(filename=file_adress).extractall(directory=f"/home/container/users_file/{chat_id}/files/")
+        pyunpack.Archive(filename=file_adress).extractall(directory=f"/home/container/users_file/{chat_id}/")
     else:
-        pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"/home/container/{chat_id}/")
+        pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"/home/container/users_file/{chat_id}/")
     
-    files_list = listdir(f"/home/container/users_file/{chat_id}/files/")
+    files_list = listdir(f"/home/container/users_file/{chat_id}/")
     
     final_file_adresses = []
     for file in files_list:
         
-        final_file_adresses.append(f"/home/container/users_file/{chat_id}/files/{file}")
+        final_file_adresses.append(f"/home/container/users_file/{chat_id}/{file}")
         
     return final_file_adresses
