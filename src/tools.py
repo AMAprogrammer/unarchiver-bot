20e33121c76ae30762377abbf2f45009e6678140
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-23 12:12:32 +0330

    commit-6

--- src/tools.py
+++ src/tools.py
@@ -1,17 +1,15 @@
-import patoolib
 from os import listdir
-
-
+import pyunpack
 def unarchive(chat_id, file_name, password):
 
     file_adress = f"/home/container/downloads/{file_name}"
     
     if password == "the file is not locked":
-        patoolib.extract_archive(archive=file_adress, outdir=f"/home/container/users_file/{chat_id}/files/")
+        pyunpack.Archive(filename=file_adress).extractall(directory=f"/home/container/users_file/{chat_id}/files/")
     else:
-        patoolib.extract_archive(archive=file_adress, outdir=f"/home/container/users_file/{chat_id}/files/",password=password)
+        pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"/home/container/{chat_id}/")
     
-    files_list = listdir(f"/home/container/users_file{chat_id}/files/")
+    files_list = listdir(f"/home/container/users_file/{chat_id}/files/")
     
     final_file_adresses = []
     for file in files_list:
