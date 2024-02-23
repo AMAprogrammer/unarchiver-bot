282c40aa3dd9208a4f5d7ad0165e6ef0bf95c797
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-23 12:51:40 +0330

    commit-6

--- src/tools.py
+++ src/tools.py
@@ -1,16 +1,19 @@
-from os import listdir, mkdir
+import os
 import pyunpack
 def unarchive(chat_id, file_name, password):
 
     file_adress = f"/home/container/downloads/{file_name}"
-    mkdir(path=f"home/container/users_file/{chat_id}")
+    
+    parent_dir = "/home/container/users_file"
+    path = os.path.join(parent_dir, str(chat_id))
+    os.mkdir(path)
     
     if password == "the file is not locked":
         pyunpack.Archive(filename=file_adress).extractall(directory=f"/home/container/users_file/{chat_id}/")
     else:
         pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"/home/container/users_file/{chat_id}/")
     
-    files_list = listdir(f"/home/container/users_file/{chat_id}/")
+    files_list = os.listdir(f"/home/container/users_file/{chat_id}/")
     
     final_file_adresses = []
     for file in files_list:
