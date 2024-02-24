30f56312b3f69df272032b3548f0152d88e658a4
Amirhossein Alizadeh <amirhosseinalizadeh992@gmail.com>
2024-02-24 14:14:25 +0330

    On branch master 	hard reset to commit-6
     Changes to be committed:
    	modified:   src/tools.py

--- src/tools.py
+++ src/tools.py
@@ -4,16 +4,15 @@ def unarchive(chat_id, file_name, password):
 
     file_adress = f"/home/container/downloads/{file_name}"
     
-    parent_dir = "/home/container/users_file"
-    path = os.path.join(parent_dir, str(chat_id))
+    path = f"/home/container/users_file/{str(chat_id)}"
     os.mkdir(path)
     
     if password == "the file is not locked":
-        pyunpack.Archive(filename=file_adress).extractall(directory=f"/home/container/users_file/{chat_id}/")
+        pyunpack.Archive(filename=file_adress).extractall(directory=f"{path}/")
     else:
-        pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"/home/container/users_file/{chat_id}/")
+        pyunpack.Archive(filename=file_adress,password=password).extractall(directory=f"{path}/")
     
-    files_list = os.listdir(f"/home/container/users_file/{chat_id}/")
+    files_list = os.listdir(f"{path}/")
     
     final_file_adresses = []
     for file in files_list:
